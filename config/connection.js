1a9d4280984517ebf904a6dee7b2708e1060620a
albarrank <albarrankevin97@gmail.com>
2019-05-11 13:38:34 -0700

    api and html routes setup

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+var mysql = require("mysql");
+require("dotenv").config();
+
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "exampledb"
+  });
+}
+
+connection.connect(function(err) {
+  if (err) {
+    console.log("this did not go well");
+    console.log(err);
+    return;
+  }
+  console.log("mysql connected: ", connection.threadId);
+});
+
+module.exports = connection;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,11 @@
 var db = require("../models");
+var express = require("express");
+var apiRoute = express.Router();
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+apiRoute = express();
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+apiRoute.use(express.urlencoded({ extended: true }));
+apiRoute.use(express.json());
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-};
+module.exports = apiRoute;
+
